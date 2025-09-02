9b7cb9ab833d481228c215fb117b95c8115f79e1
Muneeb Rehman <rehman19h@gmail.com>
2025-09-02 14:14:06 +0500

    disply Genres

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const GameCardContainer = ({ children }: Props) => {
   return (
-    <Box width={"300px"} borderRadius={10} overflow={"hidden"} margin={"20px"}>
+    <Box borderRadius={10} overflow={"hidden"}>
       {children}
     </Box>
   );
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -6,13 +6,13 @@ import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
   const { data, error, isLoading } = useGames();
-  const skeletons = [1, 2, 3, 4];
+  const skeletons = [1, 2, 3, 4, 5];
   return (
     <>
       {error && <p>{error}</p>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        spacing={10}
+        spacing={3}
         padding={10}
       >
         {isLoading &&
